6abfa81ea6ae9c6f1decbe7176934b86ecd8e918
oudeqi <496997374@qq.com>
2018-03-09 23:06:46 +0800

    share complate

--- app/pages/share/share.js
+++ app/pages/share/share.js
@@ -1,12 +1,9 @@
 // __webpack_public_path__ = process.env.PUBLIC_PATH;
-console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV)
 import 'bootstrap/dist/css/bootstrap.css'
-import './share.css';
-
-// 测试模拟数据
-$.get('http://localhost:3005/employee/123', function(res){
-    console.log(res);
-})
+import './share.css'
+import accounting from 'accounting'
+import { BASE_URL } from '../config'
 
 const getQueryString = function (name) {
     let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i'),
@@ -18,12 +15,52 @@ const getQueryString = function (name) {
 }
 
 let id = getQueryString('id')
+let pageData = null
 if (!id) {
     $('#page-warpper').html(`
         <p style="text-align: center;padding: 20px 10px;color: #ccc;">does not exist</p>
     `)
 } else {
-    /* $('#page-warpper').html(`
+    $('#page-warpper').html(`
         <p style="text-align: center;padding: 20px 10px;color: #ccc;">Loading...</p>
-    `) */
+    `)
+    $.get(BASE_URL + '/commodity?id=' + id).done(function(result){
+        let res = JSON.parse(result)
+        if (res.code === 200) {
+            pageData = JSON.parse(result).data.commodity
+            console.log(pageData)
+            $('#page-warpper').html(`
+                <div class="product__video">
+                    <div class="product__video--warpper">
+                        <video class="video" controls src="${pageData.video}" poster="${pageData.picture}">
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
+                <div class="product__head">
+                    <div class="l">
+                        <h3>${pageData.name}</h3>
+                        <p>
+                            <strong>Rp. ${accounting.formatNumber(pageData.price)}</strong>
+                            <span>/</span>
+                            <span>average price</span>
+                        </p>
+                    </div>
+                    <div class="r">
+                        <a href="${pageData.booking}" target="_blank">Visit Website</a>
+                    </div>
+                </div>
+                <div class="product__body">
+                    ${pageData.description}
+                </div>
+            `)
+        }
+
+    }).fail(function (error) {
+        console.log(error)
+        $('#page-warpper').html(`
+             <p style="text-align: center;padding: 20px 10px;color: #ccc;">does not exist</p>
+         `)
+    })
 }
+
